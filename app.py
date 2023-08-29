ddbd9888cb531cb67f47e6bbc0a464caa03c0f19
Param Jani <115683209+ParamJani21@users.noreply.github.com>
2023-08-29 16:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
+from threding import Thread
+
 app=Flask(__name__)
 
 @app.route('/')
 def hello-world():
     return 'GreyMatters'
+def run():
+    app.run(host='0.0.0.0',port=8000)
 
+def keep_alive():
+    t =Thread(target=run)
+    t.start()
 if__name__=="__main__":
     app.run()
