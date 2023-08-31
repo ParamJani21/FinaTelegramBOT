e23a179503a147ec5986501db6e73f397f647f9d
Param Jani <115683209+ParamJani21@users.noreply.github.com>
2023-08-31 13:09:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app=Flask(__name__)
+from threading import Thread
+
+app = Flask('')
 
 @app.route('/')
-def hello_world():
-    return 'GreyMatters'
+def home():
+    return "Greetings. I am currently alive!"
 
-if __name__=="__main__":
-    app.run()
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
